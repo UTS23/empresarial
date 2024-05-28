345b74e030119ec5491f28d480380bdc283a1407
Brayan Moreno <112002842+UTS23@users.noreply.github.com>
2024-05-28 09:24:28 -0500

    Delete PROYECTOUTS directory

--- PROYECTOUTS/src/main/java/com/papeleria/app/repositorio/ProductoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.papeleria.app.repositorio;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.papeleria.app.entidades.Producto;
-
-public interface ProductoRepository extends MongoRepository <Producto, String> {
-   
-}
--- PROYECTOUTS/src/main/java/com/papeleria/app/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.papeleria.app.service;
-
-public interface UserService {
-	
-	public void removeSessionMessage();
-
-}
